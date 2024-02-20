ce43cff84a04e6ea5c49721bbf49ab1f48cf6b33
SpeedyQweku <princealex2024@gmail.com>
2024-02-20 08:53:39 +0000

    a rebuild of the tool

--- main.go
+++ main.go
@@ -6,20 +6,22 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 type Config struct {
-	PatFile   string
-	StdinData []string
-	PatData   []string
-	Err       error
+	PatFile string
+	UrlFile string
+	Pattern string
 }
 
 func main() {
 	var cfg Config
+	fmt.Println("Hello paramM")
 
-	flag.StringVar(&cfg.PatFile, "l", "", "file")
+	flag.StringVar(&cfg.PatFile, "p", "", "parameter file")
+	flag.StringVar(&cfg.UrlFile, "l", "", "url file")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
@@ -28,50 +30,25 @@ func main() {
 
 	flag.Parse()
 
-	if cfg.PatFile != "" {
-		cfg.PatData, cfg.Err = readFile(cfg.PatFile)
-		if cfg.Err != nil {
-			log.Fatalf("Error reading pattern file: %v", cfg.Err)
-		}
-	} else {
+	if cfg.PatFile == "" && cfg.UrlFile == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	if stdinIsPipe() {
-		cfg.Err = readStdin(&cfg)
-		if cfg.Err != nil {
-			log.Fatalf("Error reading stdin: %v", cfg.Err)
-		}
-	} else {
-		fmt.Println("stdin is not from a pipe")
+	patData, err := readFile(cfg.PatFile)
+	if err != nil {
+		log.Fatalf("Error reading pattern file: %v", err)
 	}
+	var cmd *exec.Cmd
 
-	for _, stdata := range cfg.StdinData {
-		processLine(stdata, &cfg)
-	}
-}
-
-func processLine(line string, cfg *Config) {
-	for _, pattern := range cfg.PatData {
+	for _, pattern := range patData {
 		trimmedPattern := strings.Trim(pattern, " ")
-		if !strings.HasSuffix(trimmedPattern, "=") {
-			trimmedPattern = trimmedPattern + "="
-		}
-		if strings.Contains(line, "?"+trimmedPattern) || strings.Contains(line, "&"+trimmedPattern) {
-			fmt.Printf("%s : %s\n", trimmedPattern, line)
-			break
-		}
+		cfg.Pattern = fmt.Sprintf("\\?%s=|&%s=", trimmedPattern, trimmedPattern)
+		cmd = exec.Command("grep", "-iE", cfg.Pattern, cfg.UrlFile)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
 	}
-}
-
-func readStdin(cfg *Config) error {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		line := scanner.Text()
-		cfg.StdinData = append(cfg.StdinData, line)
-	}
-	return scanner.Err()
 }
 
 func readFile(filename string) ([]string, error) {
@@ -88,9 +65,4 @@ func readFile(filename string) ([]string, error) {
 	}
 
 	return lines, scanner.Err()
-}
-
-func stdinIsPipe() bool {
-	stat, _ := os.Stdin.Stat()
-	return (stat.Mode() & os.ModeCharDevice) == 0
 }
